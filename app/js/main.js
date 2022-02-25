f7a1e177f029706f1d9f0f911cf23f32a3ed1000
Mars <KarayuMatom2@gmail.com>
2022-02-25 22:47:03 +0300

    rework tours

--- app/js/main.js
+++ app/js/main.js
@@ -21,14 +21,16 @@ $(document).ready(function () {
   // team slider
 
   $('.our-team').slick({
-    infinite: true,
+    infinite: false,
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: true,
     variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-
+    focusOnSelect: true,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
+    // centerMode: true,
+    // centerPadding: '60px',
     // responsive: [
     //   {
     //     breakpoint: 1024,
@@ -41,17 +43,15 @@ $(document).ready(function () {
     // ],
   });
 
-  // let slickItem = $('.slick-slide');
-  // $('.our-team').on('afterChange', function (event, slick, currentSlide, nextSlide) {
-  //   for (let i = 0; i < slickItem.length; i++) {
-  //     console.log(slickItem.length);
-  //     $('.team__pages-span').text(slickItem.length / slickItem.length + ' ');
-  //   }
-  // });
+  let slickItem = $('.slick-slide');
+  $('.our-team').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    // $('.team__pages-span').text(currentSlide + 1 + ' ');
+    $('.team__pages-span').text(currentSlide + 2 - 3 + ' ');
+  });
 
   // route
 
-  let mixer = mixitup('.tours');
+  // let mixer = mixitup('.tours');
 
   let routeCurrentItem = $('.route__menu-item_active');
   let routeMenuItem = $('.route__menu-item');
@@ -62,11 +62,34 @@ $(document).ready(function () {
   //   });
   // }
 
-  $('.route__menu-item').click((event) => {
-    if ($('.route__menu-item').hasClass('route__menu-item_active')) {
-      $('.route__menu-item').removeClass('route__menu-item_active');
-    } else {
-      $(this).addClass('route__menu-item_active');
-    }
-  });
+  // $('.route__menu-item').click((event) => {
+  //   if ($('.route__menu-item').hasClass('route__menu-item_active')) {
+  //     $('.route__menu-item').removeClass('route__menu-item_active');
+  //   } else {
+  //     $(this).addClass('route__menu-item_active');
+  //   }
+  // });
+
+  // $('.tours').slick({
+  //   vertical: true,
+  //   verticalSwiping: true,
+  // });
+
+  // $('#d1').click(() => {
+  //   // $($('.tours__item')[1]).addClass('slick-active');
+  //   $(tours__item[2]).addClass('slick-active');
+  // });
+  let toursItems = $('.tours__item');
+  let toursBtn = $('.route__menu-item');
+  for (let i = 0; i < toursItems.length; i++) {
+    $($(toursBtn)[i]).click(() => {
+      $(toursItems).hide();
+      $(toursBtn).removeClass('route__menu-item_active');
+      $($(toursItems)[i]).show();
+      $($(toursBtn)[i]).addClass('route__menu-item_active');
+    });
+  }
+  // for (let i = 0; i < toursBtn.length; i++) {
+  //   $($(toursBtn)[i]).click(() => {});
+  // }
 });
